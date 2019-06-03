9c110dcac5a6ffceb303558f136d5a43e3151e0f
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-06-03 08:28:14 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,24 @@ import { FormsModule} from "@angular/forms";
 import {
   MatSelectModule,
     MatRadioModule,
-    MatCardModule
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatListModule
 } from "@angular/material";
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
-    NgClassComponent
+    NgClassComponent,
+    NgIfComponent,
+    SubNgIfComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +34,10 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     FormsModule,
     MatSelectModule,
     MatRadioModule,
-    MatCardModule
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
